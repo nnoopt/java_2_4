927baf28dda200e737794a290f35018eafb1a830
nnoopt <nnoopt@gmail.com>
2021-01-28 19:39:52 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TextField textField;
+    @FXML
+    public void clickBtn1(ActionEvent actionEvent) {
+        System.out.println("java");
+    }
+
+    public void clickBtnSend(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+}
